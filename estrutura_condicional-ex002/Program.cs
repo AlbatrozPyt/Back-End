2b0ecced3a29c6bf7aff714396e51ba7deef4be5
Matheus Enrike Ramalho Ortiz <24004134870@info.local>
2023-03-31 14:35:54 -0300

    Exercicios

--- estrutura_condicional-ex002/Program.cs
+++ estrutura_condicional-ex002/Program.cs
@@ -1,21 +1,28 @@
 ﻿// Desenvolva um programa que recebe do usuário, o placar de um jogo de futebol (os gols de cada time) e informa se o resultado foi um empate, se a vitória foi do primeiro time ou do segundo time.
 
+Console.BackgroundColor = ConsoleColor.Green;
 Console.WriteLine(@$"
 ----------------------------
-       PLACAR DO JOGO
+       PLACAR DO JOGO       
 ----------------------------
 ");
-Console.WriteLine($"Qual o nome do 1° time:");
+Console.ResetColor();
+Console.Write($"Qual o nome do 1° time: ");
 string nTime1 = Console.ReadLine()!;
 
-Console.WriteLine($"Qual o nome do 2° time:");
+Console.Write($"Qual o nome do 2° time: ");
 string nTime2 = Console.ReadLine()!;
 
-Console.WriteLine($"Quantos gols o {nTime1} marcou:");
+Console.WriteLine();
+
+Console.Write($"Quantos gols o {nTime1} marcou: ");
 int time1 = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine($"Quantos gols o {nTime2} marcou:");
+Console.Write($"Quantos gols o {nTime2} marcou: ");
 int time2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
 
 if (time1 > time2) {
     Console.WriteLine($"{nTime1} Ganhou !!! e {nTime2} perdeu. Placar: {time1} x {time2}");
