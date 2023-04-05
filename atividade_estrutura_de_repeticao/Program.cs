7eeb56a21eb164091eb3ea924a79c944c92b31a2
Matheus Enrike Ramalho Ortiz <24004134870@info.local>
2023-04-05 16:17:36 -0300

    Atividade Estrutura de Repeticao

---
+++ atividade_estrutura_de_repeticao/Program.cs
@@ -0,0 +1,55 @@
+﻿string nome = "";
+int idade = -1;
+float salario = 0;
+string e = "";
+bool estadoCivil = false;
+
+do {
+    Console.Write("Digite seu nome: ");
+    nome = Console.ReadLine()!;
+
+    if (nome == "") {
+        Console.WriteLine($"ERRO, nome invalido !!!");
+    }
+
+} while(nome == "");
+
+do {
+    Console.Write("Digite sua idade: ");
+    string i = Console.ReadLine()!;
+
+    if (i != "") {
+        idade = int.Parse(i);
+    }
+
+    if (idade < 0 || idade > 100) {
+        Console.WriteLine($"ERRO, idade invalida !!!");
+    }
+
+} while(idade < 0 || idade > 100);
+
+do {
+    Console.Write("Digite seu salario: ");
+    salario = float.Parse(Console.ReadLine()!);
+
+    if (salario <= 0) {
+        Console.WriteLine($"ERRO, salario invalida !!!");
+    }
+
+} while(salario <= 0);
+
+do {
+    Console.WriteLine($"Digite seu Estado Civil, [s]-Solteiro(a) [c]-Casado(a) [v]-Viuvo(a) [d]-Divorciado(a)");
+    Console.Write("Opção: ");
+     e = Console.ReadLine()!; 
+
+    if (e == "s" || e == "c" || e == "v" || e == "d") {
+        estadoCivil = true;
+    }
+
+} while(estadoCivil == false);
+
+Console.WriteLine($"");
+
+Console.WriteLine($"Nome: {nome}, Idade: {idade} anos, Salario: R${Math.Round(salario, 2)} e Estado Civil: [{e}].");
+
--- atividade_estrutura_de_repeticao/obj/Debug/net7.0/atividade_estrutura_de_repeticao.AssemblyInfo.cs
+++ atividade_estrutura_de_repeticao/obj/Debug/net7.0/atividade_estrutura_de_repeticao.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("backend-1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("atividade_estrutura_de_repeticao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("backend-1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("backend-1")]
+[assembly: System.Reflection.AssemblyProductAttribute("atividade_estrutura_de_repeticao")]
+[assembly: System.Reflection.AssemblyTitleAttribute("atividade_estrutura_de_repeticao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
