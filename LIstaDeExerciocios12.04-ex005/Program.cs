ba3a54d11af24efbd07ca865a881e3c7b6581a0d
Matheus Enrike Ramalho Ortiz <24004134870@info.local>
2023-04-13 20:58:37 -0300

    Exercicios do 12/04 ate o ex006

---
+++ LIstaDeExerciocios12.04-ex005/Program.cs
@@ -0,0 +1,21 @@
+﻿// 5 - Escreva um algoritmo que imprima a tabuada (de 1 a 10) para os números de 1 a 10.
+// Exemplo: tabuada do 1, tabuada do 2, etc... Dica: utilize um laço dentro do outro.
+
+Console.WriteLine(@$"
+-------------------
+     TABUADA
+
+");
+
+Console.WriteLine($"");
+
+for (int x = 1; x <= 10; x++) {
+    Console.WriteLine($"  TABUADA DO {x}");
+    Console.WriteLine($"-------------------");
+    for (int y = 0; y <= 10; y++) {
+        Thread.Sleep(500);
+        Console.WriteLine($"{x} x {y} = {x*y}");
+    }
+    Console.WriteLine($"-------------------");
+    Console.WriteLine($"");
+}
---
+++ LIstaDeExerciocios12.04-ex006/Program.cs
@@ -0,0 +1,42 @@
+﻿// 6 - Escreva um algoritmo que permita a leitura dos nomes de 10 pessoas e armazene os nomes
+// lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de
+// pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver
+// entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário.
+
+static string BuscaNome(string n, string[]p) {
+    string r = "";
+    foreach (var i in p) {
+        if (i == n) {
+             Console.ForegroundColor = ConsoleColor.Green;
+             r =  $"Achei o nome {n} !!!";
+             break;
+        }
+        else {
+            Console.ForegroundColor = ConsoleColor.Red;
+             r = $"O nome {n}, NÃO foi encontrado na lista.";
+        }
+    }
+    return r;
+}
+
+string[] pessoas = new string [10];
+
+Console.WriteLine($"");
+
+for (int i = 0; i < 10; i++) {
+    Console.Write($"Digite o {i+1}º nome: ");
+    pessoas[i] = Console.ReadLine()!.ToUpper();
+}
+
+Console.WriteLine(@$"
+----------
+BUSCA NOME
+----------
+");
+
+Console.Write("Escreva um nome para eu encontrar: ");
+string busca = Console.ReadLine()!.ToUpper();
+
+Console.WriteLine($"{BuscaNome(busca,pessoas)}");
+ 
+Console.ResetColor();
