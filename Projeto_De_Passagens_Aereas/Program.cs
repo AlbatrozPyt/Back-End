c806a038e92e5fa47162f659d9bf0979f3ccf05a
Matheus Enrike Ramalho Ortiz <24004134870@info.local>
2023-04-17 20:49:02 -0300

    Exercicio Pronto-Projeto-Passagens Aereas

--- Projeto_De_Passagens_Aereas/Program.cs
+++ Projeto_De_Passagens_Aereas/Program.cs
@@ -10,7 +10,8 @@
 // Observação :  Criar ao menos uma função (Efetuar Login).
 
 // Ao cadastrar uma passagem ao final o sistema deverá perguntar se gostaria de cadastrar uma nova passagem caso contrário voltar ao menu anterior(S/N).
-string[] lista = new string[5];
+
+
 // Funcoes
 static void EfetuarLogin() {
     string r = "";
@@ -21,62 +22,31 @@ static void EfetuarLogin() {
         ---------------------------------------
         Digite a senha de acesso do Sistema: 
         ---------------------------------------
--> ");
+    -> ");
 
         r = Console.ReadLine()!;
         Console.WriteLine(r == senha ? "SENHA CORRETA" : "SENHA INCORRETA");
         
     } while(r != senha);
 }
-static void Cadastrar()  {
-
-    Console.WriteLine(@"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-    Console.WriteLine($"       Cadastro de Passagem      ");
-    Console.WriteLine(@"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-
-    Console.WriteLine($"");
-    
-    Console.Write("Nome do Passageiro: ");
-    dados[0] = Console.ReadLine()!;
-
-
-    Console.Write("Origem: ");
-    dados[1] = Console.ReadLine()!;
-
-    Console.Write("Destino: ");
-    dados[2] = Console.ReadLine()!;
-   
-
-    Console.Write("Data do Voo -> d/mm/aaaa: ");
-    dados[3] = Console.ReadLine()!;
-
-    Console.WriteLine($"");
-
-    Console.WriteLine(@"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-
-    Console.WriteLine(@$"
-    Nome do Passageiro: {dados[0]}
-    Origem: {dados[1]};
-    Destino: {dados[2]};
-    Data do Voo: {dados[3]};
-    ");
-    
-}
 
 // Declarando Variaveis
+int cont = 0;
 string opcao = "";
-
+string[] nomes = new string[5];
+string[] origem = new string[5];
+string[] destino = new string[5];
+string[] data = new string[5];
 
 // Login
 EfetuarLogin();
 
 Console.WriteLine($"");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+Console.WriteLine($"Entrando no sistema...");
+Thread.Sleep(3000);
 
 while (true) {
-    Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-    Console.WriteLine($"Entrando no sistema...");
-    Thread.Sleep(3000);
-
     Console.WriteLine($"");
 
     Console.WriteLine(@$"
@@ -92,26 +62,45 @@ while (true) {
     Console.Write($"Escolha uma opcao: ");
     opcao = Console.ReadLine()!;
 
+    if (opcao == "1") {
+        Console.WriteLine(@$"
+         =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+                CADASTRO DE PASSAGEM
+         =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        ");
 
-    switch(opcao) {
-        case "1":
-        Cadastrar();
-        break;
+        Console.Write("Digite seu nome: ");
+        nomes[cont] = Console.ReadLine()!;
 
-        case "2":
-        break;
+        Console.Write("Qual sua Origem: ");
+        origem[cont] = Console.ReadLine()!;
 
-        case "0":
-        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-        Console.Write("Finalizando programa");
-        Thread.Sleep(1000);
-        Console.Write(".");
-        Thread.Sleep(1000);
-        Console.Write(".");
-        Thread.Sleep(1000);
-        Console.Write(".");
-        Console.WriteLine($"");  
-        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-        break;
+        Console.Write("Digite seu destino: ");
+        destino[cont] = Console.ReadLine()!;
+
+        Console.Write("Digite a Data do Voo: ");
+        data[cont] = Console.ReadLine()!;
+        
+        cont++;
     }
+
+    else if (opcao == "2") {
+        for (int i = 0; i != cont; i++) {
+            Console.WriteLine($"Nome do passageiro: {nomes[i]}, Origem: {origem[i]}, Destino: {destino[i]}, Data do Voo: {data[i]};");
+        }
+    }
+
+    else if (opcao == "0") {
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.Write("Finalizando programa");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Console.WriteLine($"");  
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            break;
+}
 }
