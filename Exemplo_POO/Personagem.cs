16bc7b7645836ff4289ed4a2b4a65b395f62ad8a
Matheus Enrike Ramalho Ortiz <24004134870@info.local>
2023-04-20 21:05:55 -0300

    Fazendo o Cadastra de mai s de um personagem

--- Exemplo_POO/Personagem.cs
+++ Exemplo_POO/Personagem.cs
@@ -8,17 +8,7 @@ namespace Exemplo_POO
         public string armadura;
         public string ia;
 
+        public string[] lista = new string[3];
         // Metodos
-        public void Atacar() {
-            Console.WriteLine($"O personagem Atacou !!!");
-        }
-
-        public void Defender() {
-            Console.WriteLine($"O personagem Defendeu !!!");
-        }
-
-        public void RestaurarArmadura() {
-            Console.WriteLine($"Armadura Restaurada !!!");
-        }
     }
 }
--- Exemplo_POO/Program.cs
+++ Exemplo_POO/Program.cs
@@ -1,31 +1,104 @@
 ﻿using Exemplo_POO;
 
 Personagem p1 = new Personagem();
+string opcao = "";
+int cont = 0;
 
 do
-{
-    Console.Write($"Informe o nome do personagem: ");
-    p1.nome = Console.ReadLine()!;
+{   
+    Console.WriteLine($"");
+    
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(@$"
+    =-==-==-==-==-==-==-==-==-==-=
+        CRIAÇÃO DE PERSONAGENS
+    =-==-==-==-==-==-==-==-==-==-=
+    OPÇÕES:
+      1 - Cadastrar personagem
+      2 - Listar personagens
+      0 - Fechar programa
+    =-==-==-==-==-==-==-==-==-==-=
+    ");
+    Console.Write($"Escolha uma opção: ");
+    opcao = Console.ReadLine()!;
+    Console.ResetColor();
+    
+    while (opcao != "0" && opcao != "1" && opcao != "2")
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.WriteLine($"ERRO, Digite apenas os valores do menu !!!");
+        Console.ResetColor();
 
-    Console.Write($"Informe a idade do personagem: ");
-    p1.idade = int.Parse(Console.ReadLine()!);
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.Write($"Escolha uma opção: ");
+        opcao = Console.ReadLine()!;
+        Console.ResetColor();
+    }
 
-    Console.Write($"Informe o armadura do personagem: ");
-    p1.armadura = Console.ReadLine()!;
+    if (opcao == "1") {
+        List<Personagem> list = new List<Personagem>();
+        
+        Console.WriteLine($"");
+        
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine(@$"
+        =-==-==-==-==-==-==-==-==-==-=
+            CADASTRO DE PERSONAGENS
+        =-==-==-==-==-==-==-==-==-==-=
+        ");
+        
+        Console.Write($"Informe o nome do personagem: ");
+        p1.nome = Console.ReadLine()!;
 
-    Console.Write($"Informe a IA do personagem: ");
-    p1.ia = Console.ReadLine()!;
+        Console.Write($"Informe a idade do personagem: ");
+        p1.idade = int.Parse(Console.ReadLine()!);
 
-    List<Personagem> list = new List<Personagem>();
-    list.Add(p1);
+        Console.Write($"Informe o armadura do personagem: ");
+        p1.armadura = Console.ReadLine()!;
+
+        Console.Write($"Informe a IA do personagem: ");
+        p1.ia = Console.ReadLine()!;
+        Console.WriteLine($"=-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-=");
+
+        Console.ResetColor();
+        
+        cont++;
+    }
+
+    else if (opcao == "2") {
+        Console.WriteLine($"");
+        
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine(@$"
+        =-==-==-==-==-==-==-==-==-==-=
+            PERSONAGENS CADASTRADOS
+        =-==-==-==-==-==-==-==-==-==-=
+        ");
+        
+        List<Personagem> list = new List<Personagem>();
+        list.Add(p1);
+
+        foreach (var item in list) {
+        Console.WriteLine(@$"
+        Nome do Personagem: {item.nome}
+        Idade do Personagem: {item.idade}
+        Armadura do Personagem: {item.armadura}
+        IA do Personagem: {item.ia}
+        ");
+        }
+
+        Console.WriteLine($"=-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-=");
+        
+        Console.ResetColor();
+    }
+
+    else {
+        Console.BackgroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine($"FINALIZANDO PROGRAMA...");
+        Console.ResetColor();    
+        Thread.Sleep(2000);
+        Console.Clear();
+        break;
+    }
 } while(true);
 
-foreach (var item in list)
-{
-    Console.WriteLine(@$"
-    Nome do Personagem: {item.nome}
-    Idade do Personagem: {item.idade}
-    Armadura do Personagem: {item.armadura}
-    IA do Personagem: {item.ia}
-    ");
-}
